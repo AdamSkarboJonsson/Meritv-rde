6fe0f89cf783313310f7b107c243f11826a5b512
Adam Skärbo Jonsson <adskarbo@gmail.com>
2015-12-15 15:45:42 +0100

    Update Meritvärde.java

--- Meritvärde.java
+++ Meritvärde.java
@@ -12,8 +12,6 @@ public class Meritvärdev3
      */
     public static void main(String[] args) 
     {
-        Scanner input = new Scanner(System.in);
-        
         //Informerar användaren om vad programmet gör och uppmanar hen att börja.
         System.out.println("Välkommen till Adams meritvärdesuträknare!");
         System.out.println("Skriv in vilka kurser du har läst, hur mycket kurspoäng var och en bestod av och vilket betyg du fick i varje kurs.");
@@ -37,7 +35,7 @@ public class Meritvärdev3
         int x;
         int i;
         
-        //Satsen som samlar in alla kurser, kurspoäng och betyg.
+        //Satsen som samlar på alla kurser, kurspoäng och betyg.
         for (x = 1; x<49; x++)
         {
             if (x >= 25)
@@ -81,20 +79,79 @@ public class Meritvärdev3
         }
         ///////
         
-        //En funktion som låter användaren att visa alla kurser som hen lade in.
+        //En funktion som låter användaren visa alla kurser som hen lagt in.
         System.out.println("Skriv \"visa\" om du vill se alla kurser som du har lagt in. Tryck enter för att fortsätta.");
         svar = input.nextLine();
-
+        //String spacePoäng = "", spaceKurs = "", spaceBetyg = "";
+        String[] radKurs = new String[x], radPoäng = new String[x];
+        String[] spaceKurs = new String[x], spacePoäng = new String[x];
+        String spaceBetyg = "";
+        int y;
         if (svar.equals("visa"))
         {
-            System.out.println("------------------------------");
-
-            for(i = 1; i < x ; i++)
+            String längst = "";
+            String[] rad = new String[x];
+            
+            for(i = 1; i < x; i++)
             {
-                System.out.println("#" + i + " Kursnamn: " + kursnamn[i]);
-                System.out.println("Kurspoäng: " + kurspoäng[i]);
-                System.out.println("Betyg: " + betyg[i]);
-                System.out.println("------------------------------");
+                radKurs[i] = ("#" + i + " Kursnamn: " + kursnamn[i]);
+                radPoäng[i] = ("Kurspoäng: " + kurspoäng[i]);
+                
+                if (längst.length()<radKurs[i].length())
+                {
+                    längst = radKurs[i];
+                }
+            }
+            
+            String streck = "";
+            for(i = 1; i <(längst.length()+1); i++)
+            {
+                streck += "-";
+            }
+            
+            for (i = 1; i < x; i++)
+            {
+                spaceKurs[i] = "";
+                spacePoäng[i] = "";
+                
+                for (y = 1; y < ((streck.length() + 6) - radKurs[i].length()); y++)
+                {
+                    spaceKurs[i] += " ";
+                }
+
+                for (y = 1; y < ((streck.length() + 5 ) - radPoäng[i].length()); y++)
+                {
+                    spacePoäng[i] += " ";
+                }
+            } 
+            
+            
+            
+            
+            //for(i = 1; i <(streck.length()-9); i++)
+            //{
+            //    spacePoäng += (" ");
+            //}
+            
+            //for(i = 1; i <(streck.length() + 5); i++)
+            //{
+            //    spaceKurs += (" ");
+            //}
+            
+            for(i = 1; i < (streck.length()-3); i++)
+            {
+                spaceBetyg += (" ");
+            }
+            
+            
+            for(i = 1; i < x; i++)
+            {
+                
+                System.out.println("#" + i + " Kursnamn: " + kursnamn[i] + spaceKurs[i] + "#" + (i+1)  + " Kursnamn: " + kursnamn[i+1]);
+                System.out.println("Kurspoäng: " + kurspoäng[i] + spacePoäng[i] + " Kurspoäng: " + kurspoäng[i+1]);
+                System.out.println("Betyg: " + betyg[i] + spaceBetyg + " Betyg: " + betyg[i+1]);
+                System.out.println(streck + "     " + streck);
+                i++;
             }
             System.out.println("Psst! Du kan scrolla upp och ner i kurslistan!");
         }
@@ -136,22 +193,23 @@ public class Meritvärdev3
             
             kurspoängDoub[i] = Double.parseDouble(kurspoäng[i]);
             betygDoub[i] = Double.parseDouble(betyg[i]);
-        } 
             poäng += kurspoängDoub[i]*betygDoub[i];
             totKurspoäng += kurspoängDoub[i];
+        } 
+            
         ////////    
         
-        //Presentation av resultatet
+        //Presenterar resultatet
         poäng = Math.round((poäng/totKurspoäng) + meritpoäng);
-        System.out.println("Din poäng blev: ");
-        System.out.println("    ________ ");
-        System.out.println("   |        |");
-        System.out.println("   | " + poäng + " |");
-        System.out.println("   |________|");
+        
+        System.out.println("Din poäng blev: " + poäng);
+        //System.out.println("    ________ ");
+        //System.out.println("   |        |");
+        //System.out.println("   | " + poäng + " |");
+        //System.out.println("   |________|");
         
     }
     
-    //En funktion som underlättar inskrivningen av kurser. Kurser som Svenska 1,2,3 och Engelska 5,6,7 känner programmet igen och det fyller i kurspoängsfältet åt användaren.
     public static String kurs(String strTarget)
     {
         String strTargetFirst = strTarget;
